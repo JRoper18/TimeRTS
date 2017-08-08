ba9e1fd139b1b8f8e4574e8f4e9c6e44311e0799
JRoper18 <jackroper18@gmail.com>
2017-08-07 21:03:55 -0600

    I think I got isometric rendering working for rectangle maps.

--- TimeRTS/Game/GameRenderer.cs
+++ TimeRTS/Game/GameRenderer.cs
@@ -23,33 +23,60 @@ namespace TimeRTS.Game
             MapState map = state.getMapAtTime(0);
             Vector3 size = map.getSize();
             int rows = (int)(size.X + size.Y-1);
-            for(int i = 0; i<rows; i++) {
-                int offsetX = 0;
-                int length = i+1;
-                if (i >= size.Y) {
-                    offsetX = i - (int) size.Y + 1;
-                    length = (int) size.Y - (i - (int) size.Y + 1);
-                }
-                for(int j = 0; j<length; j++) {
-                    int posX = j + offsetX;
+            for(int i = 0; i<Math.Min(size.X, size.Y); i++) {
+                for(int j = 0; j<=i; j++) {
+                    int posX = j;
                     int posY = i - j;
-                    if(i >= size.Y) {
-                        posY = (int) size.Y - j - 1;
-                    }
                     Debug.WriteLine(posX + " " + posY);
-
-                    for(int z = (int) size.Z-1; z>=0; z--) {
-                        Vector3 currentPosition = new Vector3(posX, posY, z);
-                        GameObject currentTile = map.getTileAtPosition(currentPosition);
-                        if(currentTile == null) {
-                            continue;
-                        }
-                        RenderData currentRenderData = currentTile.GetRenderData();
-                        spriteBatch.Draw(currentRenderData.texture, pointToIsometric(currentPosition), currentRenderData.sourceRectangle, Color.White);
+                    renderColumn(posX, posY, (int)size.Z, spriteBatch, map);
+                }
+            }
+            bool isXShortestDimension = (size.X < size.Y);
+            int numberOfSameLengthRows = (int) Math.Abs(size.X - size.Y);
+            if (isXShortestDimension) {
+                for (int i = 0; i < numberOfSameLengthRows; i++) {
+                    for (int j = 0; j < size.X; j++) {
+                        int posX = j;
+                        int posY = i - j + (int) size.X;
+                        renderColumn(posX, posY, (int)size.Z, spriteBatch, map);
+                    }
+                }
+                for (int i = 0; i < size.X - 1; i++) {
+                    for (int j = 0; j < size.X - i - 1; j++) {
+                        int posY = numberOfSameLengthRows + 1 + i + j;
+                        int posX = (int)size.X - j - 1;
+                        renderColumn(posX, posY, (int)size.Z, spriteBatch, map);
+                    }
+                }
+            }
+            else {
+                for (int i = 0; i < numberOfSameLengthRows; i++) {
+                    for (int j = 0; j < size.Y; j++) {
+                        int posX = i + j + 1;
+                        int posY = (int) size.Y - 1 - j;
+                        renderColumn(posX, posY, (int) size.Z, spriteBatch, map);
+                    }
+                }
+                for(int i = 0; i<size.Y - 1; i++) {
+                    for(int j = 0; j < size.Y - i - 1; j++) {
+                        int posX = numberOfSameLengthRows + 1 + i + j;
+                        int posY = (int) size.Y - j - 1;
+                        renderColumn(posX, posY, (int)size.Z, spriteBatch, map);
                     }
                 }
             }
             spriteBatch.End();
+        }
+        private static void renderColumn(int posX, int posY, int sizeZ, SpriteBatch spriteBatch, MapState map) {
+            for (int z = sizeZ - 1; z >= 0; z--) {
+                Vector3 currentPosition = new Vector3(posX, posY, z);
+                GameObject currentTile = map.getTileAtPosition(currentPosition);
+                if (currentTile == null) {
+                    continue;
+                }
+                RenderData currentRenderData = currentTile.GetRenderData();
+                spriteBatch.Draw(currentRenderData.texture, pointToIsometric(currentPosition), currentRenderData.sourceRectangle, Color.White);
+            }
         }
         public static void LoadAllContent() {
             foreach (KeyValuePair<string, GameObjectTexture> entry in textures) {
@@ -62,7 +89,7 @@ namespace TimeRTS.Game
             int isoY = (int) (point.Y + point.Z);
             int screenX = (isoX - isoY) * TILE_WIDTH / 2;
             int screenY = (isoY + isoX) * TILE_HEIGHT / 2;
-            return new Vector2(screenX + 500, screenY);
+            return new Vector2(screenX, screenY);
         }
     }
 }
--- TimeRTS/Game/GameState.cs
+++ TimeRTS/Game/GameState.cs
@@ -12,8 +12,7 @@ namespace TimeRTS.Game
         public static GameState instance;
         private static readonly Object lockObj = new Object();
 
-        private MapState[] mapsOverTime = new MapState[] {new MapState(new Microsoft.Xna.Framework.Vector3(5,3,2))};
-        public ContentManager content;
+        private MapState[] mapsOverTime = new MapState[] {new MapState(new Microsoft.Xna.Framework.Vector3(2,10,2))};
         private GameState(){
             
         }
